067b297b58ad7ea3d1807f953feb3d708dcc458d
Fabio-Bianco <biancobfabio@gmail.com>
2025-04-03 17:47:06 +0200

    feat: add complete ServicesBar with icons, labels and styles

--- src/components/ServicesBar.jsx
+++ src/components/ServicesBar.jsx
@@ -1,7 +1,45 @@
+import './ServicesBar.css';
+import iconDigital from '../assets/img/buy-comics-digital-comics.png';
+import iconMerch from '../assets/img/buy-comics-merchandise.png';
+import iconSub from '../assets/img/buy-comics-subscriptions.png';
+import iconLocator from '../assets/img/buy-comics-shop-locator.png';
+import iconVisa from '../assets/img/buy-dc-power-visa.svg';
+
+
+const services = [
+  {
+    label: 'Digital Comics',
+    icon: iconDigital,
+  },
+  {
+    label: 'DC Merchandise',
+    icon: iconMerch,
+  },
+  {
+    label: 'Subscription',
+    icon: iconSub,
+  },
+  {
+    label: 'Comic Shop Locator',
+    icon: iconLocator,
+  },
+  {
+    label: 'DC Power Visa',
+    icon: iconVisa,
+  },
+];
+
 const ServicesBar = () => {
   return (
-    <section>
-      <p>SERVICES BAR</p>
+    <section className="services-bar">
+      <ul className="services-list">
+        {services.map((service, index) => (
+          <li key={index}>
+            <img src={service.icon} alt={service.label} />
+            <span>{service.label}</span>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
