7486f1b548047d28671b3481a733581eca64f4d2
Fabio-Bianco <biancobfabio@gmail.com>
2025-04-03 23:20:44 +0200

    feat: add logo header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
+import logo from '../assets/img/dc-logo.png';
+
 const Header = () => {
   return (
-    <header>
-      <h1>LOGO DC</h1>
+    
+    <header className="header">
+    <div className="container">
+    <img src={logo} alt="DC Logo" className="logo" />
+    </div>
+    
+
     </header>
   );
 };
