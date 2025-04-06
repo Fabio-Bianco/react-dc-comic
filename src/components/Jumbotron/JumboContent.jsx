e08396e38aed974948311cec39de2d5e24fafdaa
Fabio-Bianco <biancobfabio@gmail.com>
2025-04-07 01:21:33 +0200

    feat: add basic Jumbotron component

---
+++ src/components/Jumbotron/JumboContent.jsx
@@ -0,0 +1,13 @@
+// src/components/Jumbotron/JumboContent.jsx
+import './JumboContent.css';
+
+
+export default function JumboContent() {
+  return (
+    <section className="jumbotron">
+      <div className="jumbotron-content">
+        &rarr; Content goes here &larr;
+      </div>
+    </section>
+  );
+}
