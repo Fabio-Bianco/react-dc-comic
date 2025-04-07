4f5541a22e3eacb1fa404b51daf5796c09e4cfe8
Fabio-Bianco <biancobfabio@gmail.com>
2025-04-08 01:16:38 +0200

    #

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,14 +2,14 @@
 import './Footer.css';
 import FooterMenu from './FooterMenu';
 import FooterBottom from './FooterBottom';
-import logoDC from '../../assets/img/dc-logo-bg.png'; // 👈 importa il logo
+
 
 const Footer = () => {
   return (
     <footer className="site-footer">
       <div className="footer-top">
         <FooterMenu />
-        <img src={logoDC} alt="DC Logo" className="footer-logo-bg" /> {/* 👈 aggiunto qui */}
+        
       </div>
       <FooterBottom />
     </footer>
