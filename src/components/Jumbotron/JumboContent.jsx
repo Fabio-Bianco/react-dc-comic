85b9b04090d1d2e9f6b63acf4fc091b3b91a8ecb
Fabio-Bianco <biancobfabio@gmail.com>
2025-04-07 18:00:37 +0200

    jumbotron completed

--- src/components/Jumbotron/JumboContent.jsx
+++ src/components/Jumbotron/JumboContent.jsx
@@ -1,23 +1,31 @@
 // src/components/Jumbotron/JumboContent.jsx
 import React from 'react';
 import './JumboContent.css';
-import comics from '../../data/comics';
 import ComicCard from './ComicCard';
+import comics from '../../data/comics';
+import jumboBg from '../../assets/img/jumbotron.jpg';
+
 
 const JumboContent = () => {
-  console.log("📚 Lista comics:", comics);
-
   return (
     <section className="jumbotron">
-      <div className="jumbotron-content">
-        <h2>Current Series</h2>
 
+      {/* SEZIONE IMMAGINE IN ALTO */}
+      <div className="jumbotron-top" style={{ backgroundImage: `url(${jumboBg})` }}>
+        <button className="current-series-btn">CURRENT SERIES</button>
+      </div>
+
+      {/* SEZIONE NERA CON LE CARD */}
+      <div className="jumbotron-bottom">
         <div className="cards-container">
           {comics.map((comic) => (
-            <ComicCard key={comic.id} comic={comic} />
+            <ComicCard key={comic} comic={comic} />
           ))}
         </div>
+
+        <button className="load-more">LOAD MORE</button>
       </div>
+      
     </section>
   );
 };
