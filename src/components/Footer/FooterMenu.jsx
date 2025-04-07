99426ebac499d2ecec5cf9bd7f66d1462d3f78d4
Fabio-Bianco <biancobfabio@gmail.com>
2025-04-07 23:37:23 +0200

    Moved SHOP links under DC COMICS section in footer menu

--- src/components/Footer/FooterMenu.jsx
+++ src/components/Footer/FooterMenu.jsx
@@ -1,11 +1,35 @@
 // src/components/Footer/FooterMenu.jsx
 import footerLinks from '../../data/footerLinks';
 
-
 const FooterMenu = () => {
+  // Troviamo separatamente le sezioni
+  const dcComicsSection = footerLinks.find(section => section.title === 'DC COMICS');
+  const shopSection = footerLinks.find(section => section.title === 'SHOP');
+  const otherSections = footerLinks.filter(
+    section => section.title !== 'DC COMICS' && section.title !== 'SHOP'
+  );
+
   return (
     <div className="footer-columns">
-      {footerLinks.map((section, index) => (
+      {/* Colonna con DC COMICS + SHOP sotto */}
+      <div className="footer-column">
+        <h4>{dcComicsSection.title}</h4>
+        <ul>
+          {dcComicsSection.links.map((link, i) => (
+            <li key={`dc-${i}`}>{link}</li>
+          ))}
+        </ul>
+
+        <h4>{shopSection.title}</h4>
+        <ul>
+          {shopSection.links.map((link, i) => (
+            <li key={`shop-${i}`}>{link}</li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Le altre sezioni restano come sono */}
+      {otherSections.map((section, index) => (
         <div key={index} className="footer-column">
           <h4>{section.title}</h4>
           <ul>
@@ -20,3 +44,4 @@ const FooterMenu = () => {
 };
 
 export default FooterMenu;
+
